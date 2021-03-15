d6521d966ee71f9b4a9e390c8af8a9ddbbc8553d
YuhBish <elias.stromberg@elev.ga.ntig.se>
2021-03-15 12:45:10 +0100

    Loggbok Skeleton

---
+++ loggbok.py
@@ -0,0 +1,12 @@
+while True:
+    choise=input(" [1] Läs logg\n [2] Skriv logg\n [3] Rensa logg\n [4] Avsluta\n")
+    if choise=="1":
+        print("Alla loggar ")
+    elif choise=="2":
+         print("Skriv logg ")
+    elif choise=="3":
+        print("Skriv logg ")
+    elif choise=="4":
+        print("Hejdå! ")
+    else:
+        print("Felaktig inmatning")
